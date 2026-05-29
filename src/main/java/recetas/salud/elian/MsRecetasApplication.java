b6ea2fde00c32a0606ac3dd4adc02262f7fdcb16
Elian Salomon <floreschavezeliansalomon@gmail.com>
2026-05-29 00:14:30 -0600

    feat: initial commit with ms-recetas structure

---
+++ src/main/java/recetas/salud/elian/MsRecetasApplication.java
@@ -0,0 +1,13 @@
+package recetas.salud.elian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRecetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/recetas/salud/elian/MsRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package recetas.salud.elian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
